87132cd760b4aeebd5ee78fc9115e90d2784b975
Prem Sai <premsai200804@gmail.com>
2026-07-10 15:23:53 +0530

    feat(constants): add company at-a-glance facts

---
+++ src/constants/companyFacts.ts
@@ -0,0 +1,5 @@
+export const COMPANY_FACTS = [
+  { label: 'Founded', value: 'July 2026' },
+  { label: 'Incubated by', value: 'CUTM' },
+  { label: 'Structure', value: 'A venture of Gramtarang Group' },
+] as const;
