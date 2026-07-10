101d8796f3d4e42b54e04c4805e1d16e99042064
Prem Sai <premsai200804@gmail.com>
2026-07-10 15:23:53 +0530

    feat(constants): add export market list

---
+++ src/constants/exportMarkets.ts
@@ -0,0 +1,12 @@
+export const EXPORT_MARKETS = [
+  'Australia',
+  'USA',
+  'Brazil',
+  'Iran',
+  'Indonesia',
+  'Malaysia',
+  'Madagascar',
+  'Timor-Leste',
+  'Middle East',
+  'Europe',
+] as const;
